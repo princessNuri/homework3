c8864ff0361fdd7e5e8a127d2b10491201ae9eed
princessnuri <98942222+princessnuri@users.noreply.github.com>
2022-05-06 15:58:43 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,17 @@ public class Main {
     public static void main(String[] args) {
         double[] numbers = {1.5,6.7,-9.0,-3.7,12.4,4.75,2.55,8.12,5.5,6.12,7.3,1.95,9.34,7.88,3.33};
         double counter = 0;
+        double quantityCounter = 0;
         for (double item:numbers) {
             counter+=item;
+            quantityCounter++;
             if (item<=0){
                 counter=0;
+                quantityCounter=0;
                 continue;
             }
         }
-        System.out.println(counter/ numbers.length);
+        System.out.println(counter/ quantityCounter);
         int [] myArray= {2,5,3,1,4};
         System.out.println("//////////////");
         selectionSort(myArray);
